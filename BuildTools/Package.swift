799923923220deaf813b82e9cc172ac1b2d82489
AhmadAzam <ahmadazam950@gmail.com>
2022-05-20 07:20:34 +0500

    SwiftLint added through SPM
    Added SwiftFormat through SPM and added run script in build phases.
    Added explicit path to config files for swiftlint and swiftformat.
    Removed Podfile

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "BuildTools",
     platforms: [.macOS(.v10_11)],
     dependencies: [
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.0"), .package(url: "https://github.com/realm/SwiftLint.git", .upToNextMajor(from: "0.47.1")),
     ],
     targets: [.target(name: "BuildTools", path: "")]
 )
