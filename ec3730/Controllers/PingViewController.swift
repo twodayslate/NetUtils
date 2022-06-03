b2c82a3b78f037b6fbce9286ee7fa67b80e7ee84
twodayslate <zac@gorak.us>
2022-06-03 16:42:19 -0400

    Add comment to quick fix

--- ec3730/Controllers/PingViewController.swift
+++ ec3730/Controllers/PingViewController.swift
@@ -309,6 +309,8 @@ struct PingSwiftUIViewController: View {
                             }.padding()
                         }
                         .background(Color.black.ignoresSafeArea(.all, edges: .horizontal))
+                        // Temporary fix until something like https://stackoverflow.com/a/65218077 is implemented
+                        // See !92 for more information
                         .padding(.top, 0.15)
                         .onTapGesture {
                             dismissKeyboard = UUID()
