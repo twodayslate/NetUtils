6b3c9608d306aede58f8b260adb39c8f93507139
twodayslate <zac@gorak.us>
2022-10-03 21:36:43 -0400

    Add more API endpoints

--- cloudflare_worker.js
+++ cloudflare_worker.js
@@ -3,7 +3,15 @@ const apiKeyKeyMap = new Map([
   ["whoisXml", "apiKey"],
   ["whoisXmlReputation", "apiKey"],
   ["whoisXmlBalance", "apiKey"],
-  ["monapi", "userKey"]
+  ["monapi", "userKey"],
+  ["ipifyGeo", "apiKey"],
+  ["ipifyVpnProxy", "apiKey"],
+  ["shodan", "key"],
+  ["whoisXmlDomainAvailability", "apiKey"],
+  ["whoisXmlIpGeo", "apiKey"],
+  ["whoisXmlWebsiteCategorization", "apiKey"],
+  ["whoisXmlWebsiteContact", "apiKey"],
+  ["whoisXmlEmailVerification", "apiKey"]
 ])
 
 const apiKeyMap = new Map([
@@ -11,7 +19,15 @@ const apiKeyMap = new Map([
   ["whoisXml", WHOIS_XML_API_KEY],
   ["whoisXmlReputation", WHOIS_XML_API_KEY],
   ["whoisXmlBalance", WHOIS_XML_API_KEY],
-  ["monapi", MONAPI_KEY]
+  ["monapi", MONAPI_KEY],
+  ["ipifyGeo", IPIFY_KEY],
+  ["ipifyVpnProxy", IPIFY_KEY],
+  ["shodan", SHODAN_API_KEY],
+  ["whoisXmlDomainAvailability", WHOIS_XML_API_KEY],
+  ["whoisXmlIpGeo", WHOIS_XML_API_KEY],
+  ["whoisXmlWebsiteCategorization", WHOIS_XML_API_KEY],
+  ["whoisXmlWebsiteContact", WHOIS_XML_API_KEY],
+  ["whoisXmlEmailVerification", WHOIS_XML_API_KEY]
 ]);
 
 const apiHostMap = new Map([
@@ -19,7 +35,16 @@ const apiHostMap = new Map([
   ["whoisXml", "www.whoisxmlapi.com"],
   ["whoisXmlBalance", "user.whoisxmlapi.com"],
   ["whoisXmlReputation", "domain-reputation.whoisxmlapi.com"],
-  ["monapi", "api.monapi.io"]
+  ["monapi", "api.monapi.io"],
+  ["ipify", "api64.ipify.org"],
+  ["ipifyGeo", "geo.ipify.org"],
+  ["ipifyVpnProxy", "vpn-proxy-detection.ipify.org"],
+  ["shodan", "api.shodan.io"],
+  ["whoisXmlDomainAvailability", "domain-availability.whoisxmlapi.com"],
+  ["whoisXmlIpGeo", "ip-geolocation.whoisxmlapi.com"],
+  ["whoisXmlWebsiteCategorization", "website-categorization.whoisxmlapi.com"],
+  ["whoisXmlWebsiteContact", "website-contacts.whoisxmlapi.com"],
+  ["whoisXmlEmailVerification", "emailverification.whoisxmlapi.com"]
 ]);
 
 addEventListener('fetch', event => {
@@ -66,75 +91,3 @@ async function handleRequest(request) {
   
   return new Response("{ \"error\": \"No API specified\" }", {status: 500});
 }
-
-
-
-// async function handleRequest(request) {
-//   let requestURL = new URL(request.url)
-//   let path = requestURL.pathname.split('/redirect')[1]
-//   let location = redirectMap.get(path)
-//   if (location) {
-//     return Response.redirect(location, 301)
-//   }
-//   // If in map, return the original request
-//   return fetch(request)
-// }
-// addEventListener('fetch', async event => {
-//   event.respondWith(handleRequest(event.request))
-// })
-// const externalHostname = 'workers-tooling.cf'
-// const redirectMap = new Map([
-//   ['/bulk1', 'https://' + externalHostname + '/redirect2'],
-//   ['/bulk2', 'https://' + externalHostname + '/redirect3'],
-//   ['/bulk3', 'https://' + externalHostname + '/redirect4'],
-//   ['/bulk4', 'https://google.com'],
-// ])
-
-
-// async function handleRequest(request) {
-//   /**
-//    * Best practice is to only assign new properties on the request
-//    * object (i.e. RequestInit props) through either a method or the constructor
-//    */
-//   let newRequestInit = {
-//     // Change method
-//     method: 'POST',
-//     // Change body
-//     body: JSON.stringify({ bar: 'foo' }),
-//     // Change the redirect mode.
-//     redirect: 'follow',
-//     //Change headers, note this method will erase existing headers
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     // Change a Cloudflare feature on the outbound response
-//     cf: { apps: false },
-//   }
-//   // Change URL
-//   let url = someUrl
-//   // Change just the host
-//   url = new URL(url)
-//   url.hostname = someHost
-//   // Best practice is to always use the original request to construct the new request
-//   // thereby cloning all the attributes, applying the URL also requires a constructor
-//   // since once a Request has been constructed, its URL is immutable.
-//   const newRequest = new Request(url, new Request(request, newRequestInit))
-//   // Set headers using method
-//   newRequest.headers.set('X-Example', 'bar')
-//   newRequest.headers.set('Content-Type', 'application/json')
-//   try {
-//     return await fetch(newRequest)
-//   } catch (e) {
-//     return new Response(JSON.stringify({ error: e.message }), { status: 500 })
-//   }
-// }
-// addEventListener('fetch', event => {
-//   event.respondWith(handleRequest(event.request))
-// })
-// /**
-//  * Example someHost is set up to return raw JSON
-//  * @param {string} someUrl the URL to send the request to, since we are setting hostname too only path is applied
-//  * @param {string} someHost the host the request will resolve too
-//  */
-// const someHost = 'example.com'
-// const someUrl = 'https://foo.example.com/api.js'
