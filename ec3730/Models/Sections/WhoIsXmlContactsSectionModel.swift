49050fb33d6f634c0e7004715685c3b924d5d5b8
Chetan <techwinlabs@gmail.com>
2022-10-20 10:27:45 +0530

    Add WHOIS XML API Website Contacts API endpoint #74 #112 - Fork's code review feedback

--- ec3730/Models/Sections/WhoIsXmlContactsSectionModel.swift
+++ ec3730/Models/Sections/WhoIsXmlContactsSectionModel.swift
@@ -25,23 +25,23 @@ class WhoIsXmlContactsSectionModel: HostSectionModel {
         latestData = copyData
         dataToCopy = String(data: copyData, encoding: .utf8)
         var rows = [CopyCellRow]()
-        
+
         if let names = records.companyNames {
             let row = CopyCellRow(title: "Company Names", content: names.joined(separator: "\n"))
             rows.append(row)
         }
-        
+
         rows.append(CopyCellRow(title: "Title", content: records.meta?.title))
-        
+
         rows.append(CopyCellRow(title: "Description", content: records.meta?.metaDescription))
 
         if let postal = records.postalAddresses {
             let row = CopyCellRow(title: "Postal Addresses", content: postal.joined(separator: "\n"))
             rows.append(row)
         }
-        
+
         rows.append(CopyCellRow(title: "Country code", content: records.countryCode))
-        
+
         if let emails = records.emails {
             var emailsArr = [String]()
             for email in emails {
@@ -51,7 +51,7 @@ class WhoIsXmlContactsSectionModel: HostSectionModel {
             let row = CopyCellRow(title: "Emails", content: emailsArr.joined(separator: "\n"))
             rows.append(row)
         }
-        
+
         if let phones = records.phones {
             var phoneArr = [String]()
             for phone in phones {
@@ -62,7 +62,7 @@ class WhoIsXmlContactsSectionModel: HostSectionModel {
             let row = CopyCellRow(title: "Phone", content: phoneArr.joined(separator: "\n"))
             rows.append(row)
         }
-        
+
         rows.append(CopyCellRow(title: "Domain name", content: records.domainName))
 
         rows.append(CopyCellRow(title: "Website responed", content: "\(records.websiteResponded ?? false)"))
@@ -72,23 +72,23 @@ class WhoIsXmlContactsSectionModel: HostSectionModel {
         if let facebook = records.socialLinks?.facebook {
             socialRows.append(CopyCellRow(title: "Facebook", content: facebook))
         }
-        
+
         if let twitter = records.socialLinks?.twitter {
             socialRows.append(CopyCellRow(title: "Twitter", content: twitter))
         }
-        
+
         if let instagram = records.socialLinks?.instagram {
             socialRows.append(CopyCellRow(title: "Instagram", content: instagram))
         }
-        
+
         if let linkedIn = records.socialLinks?.linkedIn {
             socialRows.append(CopyCellRow(title: "LinkedIn", content: linkedIn))
         }
-        
+
         content.append(CopyCellView(title: "Contacts", rows: rows))
-        
+
         content.append(CopyCellView(title: "Social Links", rows: socialRows))
-        
+
         return copyData
     }
 
@@ -113,7 +113,7 @@ class WhoIsXmlContactsSectionModel: HostSectionModel {
         }
 
         let response: WhoIsXmlContactsResult = try await WhoisXml.contactsService.query(["domain": host])
-      
+
 //        guard let record = response.dnsData.dnsRecords else {
 //            throw URLError(URLError.badServerResponse)
 //        }
