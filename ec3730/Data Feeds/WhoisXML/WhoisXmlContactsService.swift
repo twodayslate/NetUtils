27c9085f07a260abd19beb34dccc9098738df4b5
admin <>
2022-11-01 14:01:40 +0530

    Add WHOIS XML API Website Contacts API endpoint #74 #112

--- ec3730/Data Feeds/WhoisXML/WhoisXmlContactsService.swift
+++ ec3730/Data Feeds/WhoisXML/WhoisXmlContactsService.swift
@@ -15,14 +15,16 @@ class WhoisXmlContactsService: WhoisXMLService {
         guard let userData = userData, let userInput = userData["domain"] as? String, let domain = userInput.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
             return nil
         }
-        
-        let params = [URLQueryItem(name: "domainName", value: domain),
+
+        var params = [URLQueryItem(name: "domainName", value: domain),
                       URLQueryItem(name: "outputFormat", value: "JSON"),
                       URLQueryItem(name: "type", value: "_all"),
                       URLQueryItem(name: "api", value: "whoisXmlWebsiteContact"),
                       URLQueryItem(name: "identifierForVendor", value: UIDevice.current.identifierForVendor?.uuidString),
                       URLQueryItem(name: "bundleIdentifier", value: Bundle.main.bundleIdentifier)]
-        
+        if let key = WhoisXml.current.userKey {
+            params.append(URLQueryItem(name: "apiKey", value: key))
+        }
         return WhoisXml.Endpoint(host: "api.netutils.workers.dev", path: "/api/v1", queryItems: params)
     }
 }
